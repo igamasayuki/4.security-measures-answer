041826911f996da9055b57458ed66793c752b522
伊賀将之 <igamasayuki@igamasayukinoMacBook-puro.local>
2019-06-12 11:41:09 +0900

    CSRFを対策しました

--- src/main/java/com/example/controller/BbsController.java
+++ src/main/java/com/example/controller/BbsController.java
@@ -2,6 +2,7 @@ package com.example.controller;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 import javax.servlet.http.HttpSession;
 
@@ -38,6 +39,11 @@ public class BbsController {
 		}
 		session.setAttribute("articleList", articleList);	
 		
+		// 追加　開始
+		String token = UUID.randomUUID().toString();
+		session.setAttribute("token", token);
+		// 追加　終了
+		
 		// 投稿画面へ遷移
 		return "bbs";
 	}
@@ -49,15 +55,29 @@ public class BbsController {
 	 *            リクエストパラメータのname
 	 * @param body
 	 *            リクエストパラメータのbody
+	 * @param token
+	 *            ワンタイムトークン				
 	 * @return 掲示板画面
 	 */
 	@RequestMapping("/postArticle")
-	public String postArticle(String name, String body) {
+	public String postArticle(String name, String body, String token) {
+		// 追加 開始
+		String tokenInSession = (String) session.getAttribute("token");
+		if (token == null || !(token.equals(tokenInSession))) {
+			return "error";
+		}
+		// 追加　終了
+		
 		@SuppressWarnings("unchecked")
 		List<Article> articleList = (List<Article>) session.getAttribute("articleList");
 		Article article = new Article(name, body);
 		ArticleService articleService = new ArticleService();
 		articleService.postArticle(articleList, article);
+		
+		// 追加 開始
+		session.removeAttribute("token");
+		// 追加　終了
+		
 		return "redirect:/bbs/show";
 	}
 
